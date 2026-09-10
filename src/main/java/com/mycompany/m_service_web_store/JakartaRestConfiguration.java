570dad54604b8ea1f2417103c3e0d8837f375f22
maurock77 <mauro.ahenriquezperez@outlook.com>
2026-09-10 07:53:50 -0300

    se crea rama interface y se agrega index.jsp (se elimina archivo html)

---
+++ src/main/java/com/mycompany/m_service_web_store/JakartaRestConfiguration.java
@@ -0,0 +1,13 @@
+package com.mycompany.m_service_web_store;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+/**
+ * Configures Jakarta RESTful Web Services for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JakartaRestConfiguration extends Application {
+    
+}
