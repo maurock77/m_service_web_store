9403fc0d2a7a0bb4ca816e89791713f46d9976d9
maurock77 <mauro.ahenriquezperez@outlook.com>
2026-09-17 00:03:25 -0300

    se completa clase curso, sus atributos, constructores y métodos getters & setters

--- src/main/java/com/mycompany/m_service_web_store/modelo/Curso.java
+++ src/main/java/com/mycompany/m_service_web_store/modelo/Curso.java
@@ -10,4 +10,59 @@ package com.mycompany.m_service_web_store.modelo;
  */
 public class Curso {
     
+    private int id;
+    private String nombre;
+    private String descripcion;
+    private int duracion;
+    
+    // constructores
+    public Curso() {
+        
+    }
+    
+    public Curso(int id, String nombre, String descripcion, int duracion) {
+        this.id = id;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.duracion = duracion;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public int getDuracion() {
+        return duracion;
+    }
+
+    public void setDuracion(int duracion) {
+        this.duracion = duracion;
+    }
+
+    @Override
+    public String toString() {
+        return "Curso{" + "id=" + id + ", nombre=" + nombre + ", descripcion=" + descripcion + ", duracion=" + duracion + '}';
+    }
+    
+    
 }
