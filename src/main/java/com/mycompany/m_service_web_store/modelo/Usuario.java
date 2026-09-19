823758dab8c43656f27316ca3a62a4f7a8a9b441
maurock77 <mauro.ahenriquezperez@outlook.com>
2026-09-18 21:50:38 -0300

    se agrega relación inversa con UsuarioCurso en la clase Usuario

--- src/main/java/com/mycompany/m_service_web_store/modelo/Usuario.java
+++ src/main/java/com/mycompany/m_service_web_store/modelo/Usuario.java
@@ -4,17 +4,35 @@
  */
 package com.mycompany.m_service_web_store.modelo;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author mauricio
  */
+@Entity
+@Table(name = "usuario")
 public class Usuario {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String nombre;
     private String correo;
     private String passwordHash;
     private String fotoUrl;
+    
+    // relación inversa hacia UsuarioCurso
+@OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL, orphanRemoval = true)
+private List<UsuarioCurso> cursosInscritos = new ArrayList<>();
     
     // constructores
 public Usuario() {
